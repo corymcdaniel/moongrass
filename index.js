2446863d33c3d41dfdab2605ddb06be5525c1333
Cory McDaniel <cory@fullstory.com>
2024-09-17 15:17:28 -0700

    move methods into index to test a fix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const mapResultsToResponse = require('./tools/mapResultsToResponse');
-
 const express = require('express');
 const path = require('path');
 const puppeteer = require('puppeteer-core');
@@ -14,6 +12,40 @@ let mappedReport;
 // const runLighthouse = async (url) => {
 //   return {report: fsReport};
 // }
+
+const performance = (results) => {
+  /*
+  'engagement-and-retention',
+    'user-frustration-mitigation',
+    'practices-and-efficiency',
+   */
+  const enr = (results.categories?.['engagement-and-retention']?.score || 0) * 0.3;
+  const ufm = (results.categories?.['user-frustration-mitigation']?.score || 0) * 0.5;
+  const pne = (results.categories?.['practices-and-efficiency']?.score || 0) * 0.2;
+  return enr + ufm + pne;
+};
+
+const security = (results) => results.categories?.['security-and-trust']?.score || 0;
+
+const seo = (results) => results.categories?.seo?.score || 0;
+
+const dataRichness = (results) => results.categories?.['data-richness']?.score || 0;
+
+const mapResultsToResponse = (results) => {
+  if (!results || !results.categories) return null;
+
+  const response = {
+    categories: [
+      { performance: performance(results) },
+      { 'best-practices': security(results) },
+      { seo: seo(results) },
+      { accessibility: dataRichness(results) },
+    ],
+  };
+
+  console.log(`Sending score results: ${response}`);
+  return response;
+};
 
 const runLighthouse = async (url) => {
   //return {report: fsReport};
