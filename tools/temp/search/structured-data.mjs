45f5cfbc7665432b3dd36d72c7d66aa77e20f54b
Cory McDaniel <cory@fullstory.com>
2024-09-23 17:44:33 -0700

    one by one check

--- tools/temp/search/structured-data.mjs
+++ tools/temp/search/structured-data.mjs
@@ -2,9 +2,9 @@ const auditRefs = [
   { id: 'hreflang', weight: 1, group: 'search-visibility-and-navigation' },  // Hreflang for language and regional targeting
   { id: 'canonical', weight: 1, group: 'search-visibility-and-navigation' },  // Canonical URL
   { id: 'robots-txt', weight: 1, group: 'search-visibility-and-navigation' },  // Robots meta tags
-  //{ id: 'valid-html', weight: 1, group: 'data-richness' },  // Valid HTML structure (custom or existing audit)
-  { id: 'render-blocking-resources', weight: 1, group: 'search-visibility-and-navigation' },  // Minimize render-blocking resources
-  //{ id: 'no-mixed-content', weight: 1, group: 'data-richness' },  // Avoid mixed content
+  //inv{ id: 'valid-html', weight: 1, group: 'data-richness' },  // Valid HTML structure (custom or existing audit)
+  //{ id: 'render-blocking-resources', weight: 1, group: 'search-visibility-and-navigation' },  // Minimize render-blocking resources
+  //inv{ id: 'no-mixed-content', weight: 1, group: 'data-richness' },  // Avoid mixed content
 ]
 
 export default auditRefs;
