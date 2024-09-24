ee7c56d8a55317fceeabd514ae24a3ced51cb87c
Cory McDaniel <cory@fullstory.com>
2024-09-23 17:49:34 -0700

    Remove render-blocking-resources

--- tools/temp/data-richness/data-layer.mjs
+++ tools/temp/data-richness/data-layer.mjs
@@ -4,7 +4,7 @@ export default [
   { id: 'third-party-summary', weight: 1, group: 'data-richness' },  // Track third-party scripts interacting with the data layer
   //{ id: 'uses-secure-cookies', weight: 1, group: 'data-richness' },  // Ensure cookies related to the data layer are secure
   { id: 'bootup-time', weight: 1, group: 'data-richness' },  // JavaScript bootup time for data layer scripts
-  { id: 'render-blocking-resources', weight: 1, group: 'data-richness' },  // Avoid render-blocking data layer scripts
+  //breaking:{ id: 'render-blocking-resources', weight: 1, group: 'data-richness' },  // Avoid render-blocking data layer scripts
   { id: 'dom-size', weight: 1, group: 'data-richness' },  // Ensure the data layer doesn’t contribute to DOM bloat
   //{ id: 'x-content-type-options', weight: 1, group: 'data-richness' },  // Respect privacy regulations with data layer usage (can be custom)
 ]
--- tools/temp/search/structured-data.mjs
+++ tools/temp/search/structured-data.mjs
@@ -3,7 +3,7 @@ const auditRefs = [
   { id: 'canonical', weight: 1, group: 'search-visibility-and-navigation' },  // Canonical URL
   { id: 'robots-txt', weight: 1, group: 'search-visibility-and-navigation' },  // Robots meta tags
   //inv{ id: 'valid-html', weight: 1, group: 'data-richness' },  // Valid HTML structure (custom or existing audit)
-  //{ id: 'render-blocking-resources', weight: 1, group: 'search-visibility-and-navigation' },  // Minimize render-blocking resources
+  //breaking: { id: 'render-blocking-resources', weight: 1, group: 'search-visibility-and-navigation' },  // Minimize render-blocking resources
   //inv{ id: 'no-mixed-content', weight: 1, group: 'data-richness' },  // Avoid mixed content
 ]
 
